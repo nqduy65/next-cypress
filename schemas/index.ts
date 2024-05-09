1aa045b3ebd0de92f676ee1baafb3a4b7eaac1c7
nquduy65 <nguyenquocduy06051998@gmail.com>
2024-05-09 11:36:55 +0700

    update dashboard component

--- schemas/index.ts
+++ schemas/index.ts
@@ -2,7 +2,9 @@ import * as z from "zod";
 
 export const LoginSchema = z.object({
   email: z.string().email(),
-  password: z.string().min(1),
+  password: z.string().min(6, {
+    message: "Minimum 6 characters required",
+  }),
 });
 
 export const RegisterSchema = z.object({
